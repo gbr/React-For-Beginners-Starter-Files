664dd19b39219960253f677012b86aa89111cca6
Glenn Ryan <glennthepen@gmail.com>
2019-06-10 14:11:40 -0700

    Learned about components and the render function from ReactDOM

---
+++ catch-of-the-day/src/components/StorePicker.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class StorePicker extends Component {
+    render() {
+        return <p>Hello! I am the StorePicker</p>
+    }
+}
+
+export default StorePicker;
